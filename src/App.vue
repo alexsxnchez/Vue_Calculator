07f7a76083de0fc546c5ecbada3954bff128d920
Alexis Arevalo <alexgian1997@gmail.com>
2024-10-19 11:34:39 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,10 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
   margin: 60px 0;
 }
 </style>
